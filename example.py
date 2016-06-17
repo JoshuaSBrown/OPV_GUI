ea90860f6885140644f604c7aefb18755862c7b5
Soo Rin Park <soorinpark@engr2-2-93-241-dhcp.int.colorado.edu>
2016-06-17 11:48:03 -0600

    Added all the ones that have limited options

--- example.py
+++ example.py
@@ -1,67 +1,56 @@
 #!/usr/local/bin/python
-# -*- coding: utf-8 -*-
 
-"""
-ZetCode PyQt4 tutorial 
-
-This example shows a QtGui.QProgressBar widget.
-
-author: Jan Bodnar
-website: zetcode.com 
-last edited: September 2011
-"""
 
 import sys
+import yaml
 from PyQt4 import QtGui, QtCore
 
-class Example(QtGui.QWidget):
+class MainWindow(QtGui.QWidget):
     
     def __init__(self):
-        super(Example, self).__init__()
-        
-        self.initUI()
-        
-    def initUI(self):      
+        super(MainWindow, self).__init__()
+       
+        self.layout = QtGui.QGridLayout()
+        index = 0
+        config = self.parseConfig()
+        for key1, val1 in config.iteritems():
+            for param in val1:
+                print param
+                for key2, val2 in param.iteritems():
+                    if len(val2) == 1:
+                        print "Not Combo Box"
+                    else:
+                        print "Combo Box"
+                        
+                        # Parameter Names
+                        self.paramName = QtGui.QLabel(key2)
+                        self.layout.addWidget(self.paramName, index, 0)
 
-        self.pbar = QtGui.QProgressBar(self)
-        self.pbar.setGeometry(30, 40, 200, 25)
+                        # Combo Box for options
+                        self.comboBox = QtGui.QComboBox()
+                        for order, items in enumerate(val2):
+                            if order > 0:
+                                self.comboBox.addItems(str(items))
+                                self.layout.addWidget(self.comboBox, index, 1)
+                    index = index + 1
 
-        self.btn = QtGui.QPushButton('Start', self)
-        self.btn.move(40, 80)
-        self.btn.clicked.connect(self.doAction)
-
-        self.timer = QtCore.QBasicTimer()
-        self.step = 0
-        
+        self.setLayout(self.layout)
         self.setGeometry(300, 300, 280, 170)
-        self.setWindowTitle('QtGui.QProgressBar')
-        self.show()
-        
-    def timerEvent(self, e):
-      
-        if self.step >= 100:
-        
-            self.timer.stop()
-            self.btn.setText('Finished')
-            return
-            
-        self.step = self.step + 1
-        self.pbar.setValue(self.step)
+        self.setWindowTitle('OPV GUI')
+    
+    def parseConfig(self):
 
-    def doAction(self):
-      
-        if self.timer.isActive():
-            self.timer.stop()
-            self.btn.setText('Start')
-            
-        else:
-            self.timer.start(100, self)
-            self.btn.setText('Stop')
-        
+        with open("config.yaml", 'r') as config:
+            configFile = yaml.load(config)
+            prettyConfigFile = yaml.dump(configFile, default_flow_style = False)
+            #print prettyConfigFile
+            return configFile
+
 def main():
     
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    ex = MainWindow()
+    ex.show()
     sys.exit(app.exec_())
 
 
