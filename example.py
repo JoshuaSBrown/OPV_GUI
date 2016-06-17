429a5758a565e0060022fd9bb69e3a5fcfa7ad58
Soo Rin Park <soorinpark@engr2-2-93-241-dhcp.int.colorado.edu>
2016-06-17 12:59:11 -0600

    Added scroll to GUI

--- example.py
+++ example.py
@@ -10,34 +10,56 @@ class MainWindow(QtGui.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
        
-        self.layout = QtGui.QGridLayout()
+       	self.primaryLayout = QtGui.QVBoxLayout()
+        self.secondaryLayout = QtGui.QGridLayout()
+        
+        self.scrollArea = QtGui.QScrollArea(self)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QtGui.QWidget(self.scrollArea)
+        self.scrollAreaWidgetContents.setLayout(self.secondaryLayout)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        
+        self.loadButton = QtGui.QPushButton("Load")
+        self.primaryLayout.addWidget(self.loadButton)
+        self.primaryLayout.addWidget(self.scrollArea)
+        
         index = 0
         config = self.parseConfig()
+        
         for key1, val1 in config.iteritems():
             for param in val1:
-                print param
+                #print param
                 for key2, val2 in param.iteritems():
+                    
                     if len(val2) == 1:
-                        print "Not Combo Box"
+                        
+                        self.paramName = QtGui.QLabel(key2)
+                        self.secondaryLayout.addWidget(self.paramName, index, 0)
+
+                        self.lineEdit = QtGui.QLineEdit()
+                        self.secondaryLayout.addWidget(self.lineEdit, index, 1)
+                    
                     else:
-                        print "Combo Box"
                         
                         # Parameter Names
                         self.paramName = QtGui.QLabel(key2)
-                        self.layout.addWidget(self.paramName, index, 0)
+                        self.secondaryLayout.addWidget(self.paramName, index, 0)
 
                         # Combo Box for options
                         self.comboBox = QtGui.QComboBox()
                         for order, items in enumerate(val2):
                             if order > 0:
                                 self.comboBox.addItems(str(items))
-                                self.layout.addWidget(self.comboBox, index, 1)
+                                self.secondaryLayout.addWidget(self.comboBox, index, 1)
+                    
                     index = index + 1
 
-        self.setLayout(self.layout)
-        self.setGeometry(300, 300, 280, 170)
+        self.setLayout(self.primaryLayout)
+        self.resize(300, 600)
+        self.center()
         self.setWindowTitle('OPV GUI')
-    
+        self.show()
+
     def parseConfig(self):
 
         with open("config.yaml", 'r') as config:
@@ -45,12 +67,18 @@ class MainWindow(QtGui.QWidget):
             prettyConfigFile = yaml.dump(configFile, default_flow_style = False)
             #print prettyConfigFile
             return configFile
+            
+    def center(self):
+        
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 def main():
     
     app = QtGui.QApplication(sys.argv)
     ex = MainWindow()
-    ex.show()
     sys.exit(app.exec_())
 
 
