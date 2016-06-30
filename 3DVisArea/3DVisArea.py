779f5844bdb361754b96fee6ae651e6be02cef76
Soo Rin Park <soorinpark@engr2-2-92-208-dhcp.int.colorado.edu>
2016-06-30 16:30:22 -0600

    color added to 3D

--- 3DVisArea/3DVisArea.py
+++ 3DVisArea/3DVisArea.py
@@ -13,15 +13,19 @@ import pyqtgraph.examples
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
 from numpy import *
+from matplotlib.cm import *
 
 class MainWindow(QtGui.QWidget):
     
     def __init__(self):
         super(MainWindow, self).__init__()
         
-        self.layout = QtGui.QVBoxLayout()
+        self.mainLayout = QtGui.QHBoxLayout()
+        self.rightLayout = QtGui.QVBoxLayout()
+        self.plotLayout = QtGui.QVBoxLayout()
     
         self.plotWidget = gl.GLViewWidget()
+        self.plotWidget.setFixedSize(600, 600)
         self.plotWidget.opts['distance'] = 100
 
         xgrid = gl.GLGridItem()
@@ -49,12 +53,24 @@ class MainWindow(QtGui.QWidget):
 
         loadButton = QtGui.QPushButton("Load XYZ File")
         loadButton.clicked.connect(self.loadXYZFile)
-        
-        self.layout.addWidget(loadButton)
-        self.layout.addWidget(self.plotWidget)
+       
+        transLabel = QtGui.QLabel("Transparency")
 
-        self.setLayout(self.layout)
-        self.resize(600, 600)
+        transSlider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        transSlider.valueChanged[int].connect(self.changeTrans)
+
+        self.rightLayout.setAlignment(QtCore.Qt.AlignTop)
+        self.rightLayout.addWidget(loadButton)
+        self.rightLayout.addWidget(transLabel)
+        self.rightLayout.addWidget(transSlider)
+
+        self.plotLayout.addWidget(self.plotWidget)
+        
+        self.mainLayout.addLayout(self.plotLayout)
+        self.mainLayout.addLayout(self.rightLayout)
+
+        self.setLayout(self.mainLayout)
+        self.resize(800, 600)
         self.setWindowTitle("3D Visualtization Area")
         self.show()
 
@@ -75,126 +91,53 @@ class MainWindow(QtGui.QWidget):
         xMax = 0
         yMax = 0
         zMax = 0
-
-        
-        """
         
         #All the data points
-        
+        energy = []
         for i, j in enumerate(xyzData):
             
             xyzData[i] = xyzData[i].split('\t')
             del xyzData[i][0] # delete "C"
             pos[i] = tuple(xyzData[i][0:3])
             size[i] = .5
-            color[i] = (0.0, 1.0, 0.0, 0.5)
-        """
+            energy.append(xyzData[i][3])
+            #color[i] = (1, 0, 0, 0.5)
+   	
+        normEnergy = self.normalizeEnergy(energy)
+   		
+        for i, j in enumerate(normEnergy):
+            color[i] = hot(normEnergy[i])
 
+		# insert surfaceArea code here if needed
+
+        self.plot = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
+        self.plotWidget.addItem(self.plot)
         
-        #Just the surface area
         
-        for i, j in enumerate(xyzData):
-
-            xyzData[i] = xyzData[i].split('\t')
-
-            del xyzData[i][0]
-            del xyzData[i][3]
-            del xyzData[i][3]
-
-            #print xyzData[i]
-
-            if float(xyzData[i][0]) >= xMax:
-                xMax = float(xyzData[i][0])
-
-            if float(xyzData[i][1]) >= yMax:
-                yMax = float(xyzData[i][1])
-
-            if float(xyzData[i][2]) >= zMax:
-                zMax = float(xyzData[i][2])
-       
-
-        # plot x-plane
-        indexX1 = 0
-        for x in range(0, int(xMax) + 1):
-            for y in range(0, int(yMax) + 1):
-                
-                pos[indexX1] = (x, y, 0)
-                size[indexX1] = .5
-                color[indexX1] = (0.0, 1.0, 0.0, 0.5)
-                #print indexX1, pos[indexX1]
-                indexX1 = indexX1 + 1
+    def normalizeEnergy(self, energy):
         
-        indexX2 = indexX1
-        for x in range(0, int(xMax) + 1):
-            for y in range(0, int(yMax) + 1):
-                
-                pos[indexX2] = (x, y, zMax)
-                size[indexX2] = .5
-                color[indexX2] = (0.0, 1.0, 0.0, 0.5)
-                #print indexX2, pos[indexX2]
-                indexX2 = indexX2 + 1
+        normEnergy = []
+        energy = map(float, energy)
+        minEnergy = min(energy)
+        maxEnergy = max(energy)
+
+        for i in range(0, len(energy)):
+            
+            norm = (energy[i] - minEnergy) / (maxEnergy - minEnergy)
+            normEnergy.append(norm)
+            print norm
         
-
-        # plot y-plane
-        indexY1 = indexX2
-        for y in range(0, int(yMax) + 1):
-            for z in range(0, int(zMax) + 1):
-
-                pos[indexY1] = (0, y, z)
-                size[indexY1] = .5
-                color[indexY1] = (0.0, 1.0, 0.0, 0.5)
-                #print indexY1, pos[indexY1]
-                indexY1 = indexY1 + 1
-
-        indexY2 = indexY1
-        for y in range(0, int(yMax) + 1):
-            for z in range(0, int(zMax) + 1):
-
-                pos[indexY2] = (xMax, y, z)
-                size[indexY2] = .5
-                color[indexY2] = (0.0, 1.0, 0.0, 0.5)
-                #print indexY2, pos[indexY2]
-                indexY2 = indexY2 + 1
-
-        # plot z-plane
-        indexZ1 = indexY2
-        for z in range(0, int(zMax) + 1):
-            for x in range(0, int(xMax) + 1):
-
-                pos[indexZ1] = (x, 0, z)
-                size[indexZ1] = .5
-                color[indexZ1] = (0.0, 1.0, 0.0, 0.5)
-                #print indexZ1, pos[indexZ1]
-                indexZ1 = indexZ1 + 1
-
-
-        indexZ2 = indexZ1
-        for z in range(0, int(zMax) + 1):
-            for x in range(0, int(xMax) + 1):
-
-                pos[indexZ2] = (x, yMax, z)
-                size[indexZ2] = .5
-                color[indexZ2] = (0.0, 1.0, 0.0, 0.5)
-                #print indexZ2, pos[indexZ2]
-                indexZ2 = indexZ2 + 1
-
-        print indexZ2
-
-
-        """
-        print xMax, yMax, zMax
-        for x in range(0, int(xMax) + 1):
-            for y in range(0, int(yMax) + 1):
-                for z in range(0, int(zMax) + 1):
-                    pos[x+y+z] = (x, y, z)
-                    print pos[x+y+z]
-                    size[x+y+z] = .5
-                    color[x+y+z] = (0.0, 1.0, 0.0, 0.5)
-        """
-
-        plot = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
-        self.plotWidget.addItem(plot)
+        return normEnergy
+    
+    def changeTrans(self, value):
         
+        if value == 0:
+            self.plot.setGLOptions('opaque')
+        elif value > 0:
+            self.plot.setGLOptions('translucent')
+        else: 
+            self.plot.setGLOptions('additive')
+
 
            
 
