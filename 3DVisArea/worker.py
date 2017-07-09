0a47c63c69f68671c19e17f23a3caf351c83ef9b
Suyog Soti <suyog.soti@gmail.com>
2017-07-09 16:17:14 -0600

    xyz gui thread completed

--- 3DVisArea/worker.py
+++ 3DVisArea/worker.py
@@ -2,24 +2,29 @@ from PyQt4 import QtCore
 
 
 class Worker(QtCore.QThread):
-
-    def __init__(self, file, sig):
+    def __init__(self, file, sig, xPlaneLabel, yPlaneLabel, zPlaneLabel):
         super(Worker, self).__init__()
         self.file = file
         self.sig = sig
+        self.xPlaneLabel = xPlaneLabel
+        self.yPlaneLabel = yPlaneLabel
+        self.zPlaneLabel = zPlaneLabel
 
     def run(self):
         xyzData = list()
         if ".xyz" not in self.file:
-            self.sig.emit(xyzData, False)
+            self.sig.emit(xyzData, False, self.xPlaneLabel, self.yPlaneLabel,
+                          self.zPlaneLabel)
             return
 
         try:
             with open(self.file) as xyzFile:
                 xyzData = xyzFile.readlines()
         except IOError:
-            self.sig.emit(xyzData, False)
+            self.sig.emit(xyzData, False, self.xPlaneLabel, self.yPlaneLabel,
+                          self.zPlaneLabel)
             return
-        self.sig.emit(xyzData, True)
+        self.sig.emit(xyzData, True, self.xPlaneLabel, self.yPlaneLabel,
+                      self.zPlaneLabel)
         # print(xyzData)
         # return xyzData
--- 3DVisArea/xyzViz.py
+++ 3DVisArea/xyzViz.py
@@ -7,18 +7,23 @@ import pyqtgraph.opengl as gl
 from numpy import *
 from matplotlib.cm import *
 from worker import Worker
+
 # import itertools
 
 
 class xyzViz(QtGui.QWidget):
 
-    mysignal = QtCore.pyqtSignal(list, bool)
+    mysignal = QtCore.pyqtSignal(list, bool, QtGui.QLabel, QtGui.QLabel,
+                                 QtGui.QLabel)
+
     def __init__(self):
         super(xyzViz, self).__init__()
 
         # add init here
 
-    def loadXYZFile(self, plotWidget, plotAlreadyThere, xPlaneLabel, yPlaneLabel, zPlaneLabel):
+    def loadXYZFile(self, plotWidget, plotAlreadyThere, xPlaneLabel,
+                    yPlaneLabel, zPlaneLabel):
+        self.plotWidget = plotWidget
         widgetItems = plotWidget.items
         prevPlot = []
 
@@ -31,10 +36,16 @@ class xyzViz(QtGui.QWidget):
 #          catch:
 #             pass
 
-        xyzFileName = QtGui.QFileDialog.getOpenFileName(self, 'Load XYZ File', '../Data')
-        self.worker = Worker(xyzFileName, self.mysignal)
+        xyzFileName = QtGui.QFileDialog.getOpenFileName(
+            self, 'Load XYZ File', '../Data')
+        self.worker = Worker(xyzFileName, self.mysignal, xPlaneLabel,
+                             yPlaneLabel, zPlaneLabel)
         self.worker.start()
         self.mysignal.connect(self.printData)
+
+        if plotAlreadyThere:
+            for i in range(0, self.previousDataSize):
+                self.size[i] = 0
 
         # if ".xyz" not in xyzFileName:
         #     QtGui.QMessageBox.about(self, "Error", "Not a xyz File")
@@ -46,15 +57,10 @@ class xyzViz(QtGui.QWidget):
         # except IOError:
         #     return
 
-        # if plotAlreadyThere:
-        #     for i in range(0, self.previousDataSize):
-        #         self.size[i] = 0
-
-    def printData(self, xyzData, bool):
+    def printData(self, xyzData, bool, xPlaneLabel, yPlaneLabel, zPlaneLabel):
         self.xyzData = xyzData
         # All the data points
         energy = []
-
         del self.xyzData[:2]
         dataLen = len(self.xyzData)
 
@@ -68,7 +74,7 @@ class xyzViz(QtGui.QWidget):
 
             self.xyzData[i] = self.xyzData[i].split('\t')
             #print self.xyzData[i]
-            del self.xyzData[i][0] # delete "C"
+            del self.xyzData[i][0]  # delete "C"
             self.pos[i] = tuple(self.xyzData[i][0:3])
             self.size[i] = .5
             energy.append(self.xyzData[i][3])
@@ -90,8 +96,9 @@ class xyzViz(QtGui.QWidget):
         yPlaneLabel.setText("Y-Plane: Max %i" % self.yMaxPos)
         zPlaneLabel.setText("Z-Plane: Max %i" % self.zMaxPos)
 
-        self.plot = gl.GLScatterPlotItem(pos=self.pos, size=self.size, color=self.color, pxMode=False)
-        plotWidget.addItem(self.plot)
+        self.plot = gl.GLScatterPlotItem(
+            pos=self.pos, size=self.size, color=self.color, pxMode=False)
+        self.plotWidget.addItem(self.plot)
         plotAlreadyThere = True
         self.previousDataSize = dataLen
 
@@ -151,7 +158,6 @@ class xyzViz(QtGui.QWidget):
             self.yPlaneLE.setEnabled(True)
             self.zPlaneLE.setEnabled(True)
 
-
     def normalizeEnergy(self, energy):
 
         normEnergy = []
@@ -170,52 +176,28 @@ class xyzViz(QtGui.QWidget):
     def cubeViz(self):
 
         verts = np.array([
-            [0, 0, 0], #0
-            [0, 0, 1], #1
-            [0, 1, 0], #2
-            [0, 1, 1], #3
-            [1, 0, 0], #4
-            [1, 0, 1], #5
-            [1, 1, 0], #6
-            [1, 1, 1] #7
-            ])
-
-        faces = np.array([
-            [0, 4, 6],
-            [0, 6, 2],
-            [1, 5, 7],
-            [1, 7, 3],
-            [2, 6, 3],
-            [3, 7, 6],
-            [0, 4, 1],
-            [1, 5, 4],
-            [4, 5, 7],
-            [4, 6, 7],
-            [0, 1, 3],
-            [0, 2, 3]
-            ])
-
-        faceColors = np.array([
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3],
-            [1, 0, 0, 0.3]
-
+            [0, 0, 0],  #0
+            [0, 0, 1],  #1
+            [0, 1, 0],  #2
+            [0, 1, 1],  #3
+            [1, 0, 0],  #4
+            [1, 0, 1],  #5
+            [1, 1, 0],  #6
+            [1, 1, 1]  #7
         ])
+
+        faces = np.array([[0, 4, 6], [0, 6, 2], [1, 5, 7], [1, 7, 3],
+                          [2, 6, 3], [3, 7, 6], [0, 4, 1], [1, 5, 4],
+                          [4, 5, 7], [4, 6, 7], [0, 1, 3], [0, 2, 3]])
+
+        faceColors = np.array(
+            [[1, 0, 0, 0.3], [1, 0, 0, 0.3], [1, 0, 0, 0.3], [1, 0, 0, 0.3],
+             [1, 0, 0, 0.3], [1, 0, 0, 0.3], [1, 0, 0, 0.3], [1, 0, 0, 0.3],
+             [1, 0, 0, 0.3], [1, 0, 0, 0.3], [1, 0, 0, 0.3], [1, 0, 0, 0.3]])
 
         self.cube = verts[faces]
 
         print self.xyzData
-
-
 
     def changeShape(self, shapeCB, transSlider):
 
@@ -230,8 +212,7 @@ class xyzViz(QtGui.QWidget):
 
     def changeTrans(self, value):
 
-
-        alpha = float(value)/float(100)
+        alpha = float(value) / float(100)
         try:
             if self.coloristhere:
 
@@ -246,30 +227,31 @@ class xyzViz(QtGui.QWidget):
 
                 self.plot.setGLOptions('additive')
         except AttributeError:
-            QtGui.QMessageBox.about(self, "Error", "Must load a xyz file first.")
-
+            QtGui.QMessageBox.about(self, "Error",
+                                    "Must load a xyz file first.")
 
     def viewAllAreas(self, viewAll, xPlaneLE, yPlaneLE, zPlaneLE):
 
-            if viewAll.isChecked():
+        if viewAll.isChecked():
 
-                xPlaneLE.setEnabled(False)
-                yPlaneLE.setEnabled(False)
-                zPlaneLE.setEnabled(False)
+            xPlaneLE.setEnabled(False)
+            yPlaneLE.setEnabled(False)
+            zPlaneLE.setEnabled(False)
 
-                for i in range(0, len(self.pos)):
-                    self.size[i] = .5
+            for i in range(0, len(self.pos)):
+                self.size[i] = .5
 
-            else:
+        else:
 
-                xPlaneLE.setEnabled(True)
-                yPlaneLE.setEnabled(True)
-                zPlaneLE.setEnabled(True)
+            xPlaneLE.setEnabled(True)
+            yPlaneLE.setEnabled(True)
+            zPlaneLE.setEnabled(True)
 
     def changeViewAreas(self, plotAlreadyThere, xPlaneLE, yPlaneLE, zPlaneLE):
 
         if not plotAlreadyThere:
-            QtGui.QMessageBox.about(self, "Error", "Must load a xyz file first.")
+            QtGui.QMessageBox.about(self, "Error",
+                                    "Must load a xyz file first.")
 
         xPlaneArea = self.parseAreaInput(xPlaneLE, self.xMaxPos)
         yPlaneArea = self.parseAreaInput(yPlaneLE, self.yMaxPos)
@@ -281,7 +263,9 @@ class xyzViz(QtGui.QWidget):
                 for y in range(0, len(yPlaneArea)):
                     for z in range(0, len(zPlaneArea)):
                         self.size[i] = 0
-                        if ((self.pos[i][0] == xPlaneArea[x]) and (self.pos[i][1] == yPlaneArea[y]) and (self.pos[i][2] == zPlaneArea[z])):
+                        if ((self.pos[i][0] == xPlaneArea[x]) and
+                            (self.pos[i][1] == yPlaneArea[y]) and
+                            (self.pos[i][2] == zPlaneArea[z])):
                             visualizeThese.append(i)
 
         for j in range(0, len(visualizeThese)):
@@ -297,18 +281,17 @@ class xyzViz(QtGui.QWidget):
         temp1 = []
         temp2 = []
 
-        for i in range(0,len(planeArea)):
+        for i in range(0, len(planeArea)):
             if "-" in planeArea[i]:
 
                 temp = planeArea[i].split("-")
                 foo = int(temp[0])
                 bar = int(temp[1])
-                temp1 = range(foo, bar+1)
+                temp1 = range(foo, bar + 1)
                 planeArea.extend(temp1)
                 temp2.append(planeArea[i])
 
-
-        for i in range(0,len(temp2)):
+        for i in range(0, len(temp2)):
             planeArea.remove(temp2[i])
 
         if not planeArea[0]:
@@ -320,12 +303,13 @@ class xyzViz(QtGui.QWidget):
 
         return planeArea
 
+
 def main():
 
     app = QtGui.QApplication(sys.argv)
     ex = MainWindow()
     sys.exit(app.exec_())
 
+
 if __name__ == '__main__':
     main()
-
